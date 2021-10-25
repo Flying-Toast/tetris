4d4ef38157d3313e43d1aecc68f68004acd01e03
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-10-25 19:01:42 -0400

    more rendering refactoring

--- render.c
+++ render.c
@@ -92,7 +92,7 @@ static void render_playfield_square(SDL_Renderer *renderer, enum square sq, int 
 	render_square(renderer, sq, x * SQUARE_SIZE, (y - PLAYFIELD_VISIBLE_START) * SQUARE_SIZE, ghost);
 }
 
-static void tetris_render_current_at_y(SDL_Renderer *renderer, const struct tetris *t, int at_y, bool ghost)
+static void render_current_at_y(SDL_Renderer *renderer, const struct tetris *t, int at_y, bool ghost)
 {
 	for (int y = 0; y < SHAPE_BOUNDING_BOX_SIZE; y++) {
 		for (int x = 0; x < SHAPE_BOUNDING_BOX_SIZE; x++) {
@@ -102,14 +102,14 @@ static void tetris_render_current_at_y(SDL_Renderer *renderer, const struct tetr
 	}
 }
 
-static void tetris_render_current(SDL_Renderer *renderer, const struct tetris *t)
+static void render_current(SDL_Renderer *renderer, const struct tetris *t)
 {
-	tetris_render_current_at_y(renderer, t, t->current_y, false);
+	render_current_at_y(renderer, t, t->current_y, false);
 }
 
-static void tetris_render_ghost(SDL_Renderer *renderer, const struct tetris *t)
+static void render_ghost(SDL_Renderer *renderer, const struct tetris *t)
 {
-	tetris_render_current_at_y(renderer, t, tetris_slammed_y(t), true);
+	render_current_at_y(renderer, t, tetris_slammed_y(t), true);
 }
 
 static int tetromino_min_x(const struct tetromino *tm)
@@ -160,19 +160,19 @@ static int tetromino_max_y(const struct tetromino *tm)
 	return 0;
 }
 
-static void tetris_render_hold(SDL_Renderer *renderer, const struct tetris *t)
+static void render_hold(SDL_Renderer *renderer, const struct tetris *t)
 {
 	SDL_SetRenderDrawColor(renderer, sidebar_bg[0], sidebar_bg[1], sidebar_bg[2], 255);
 	clear_current_viewport(renderer, &hold_viewport);
 
-	SDL_Rect holdbox = {
-		.x = (VIEWPORT_HOLD_WIDTH - PIECEBOX_WIDTH) / 2,
-		.y = (WINDOW_HEIGHT - PIECEBOX_HEIGHT) / 4,
-		.w = PIECEBOX_WIDTH,
-		.h = PIECEBOX_HEIGHT
+	SDL_Rect box = {
+		.x = (VIEWPORT_HOLD_WIDTH - SHAPE_BOUNDS_PX) / 2,
+		.y = (WINDOW_HEIGHT - SHAPE_BOUNDS_PX) / 4,
+		.w = SHAPE_BOUNDS_PX,
+		.h = SHAPE_BOUNDS_PX
 	};
 	SDL_SetRenderDrawColor(renderer, piecebox_bg[0], piecebox_bg[1], piecebox_bg[2], 255);
-	SDL_RenderFillRect(renderer, &holdbox);
+	SDL_RenderFillRect(renderer, &box);
 
 	if (t->holding) {
 		for (int y = 0; y < SHAPE_BOUNDING_BOX_SIZE; y++) {
@@ -181,8 +181,8 @@ static void tetris_render_hold(SDL_Renderer *renderer, const struct tetris *t)
 				int min_y = tetromino_min_y(&t->held_tetromino);
 				int piece_width = (tetromino_max_x(&t->held_tetromino) - min_x) * SQUARE_SIZE;
 				int piece_height = (tetromino_max_y(&t->held_tetromino) - min_y) * SQUARE_SIZE;
-				int renderx = holdbox.x + ((PIECEBOX_WIDTH - piece_width) / 2) + (x * SQUARE_SIZE) - (((min_x + 1) * SQUARE_SIZE) / 2);
-				int rendery = holdbox.y + ((PIECEBOX_HEIGHT - piece_height) / 2) + (y * SQUARE_SIZE) - (((min_y + 1) * SQUARE_SIZE) / 2);
+				int renderx = box.x + ((SHAPE_BOUNDS_PX - piece_width) / 2) + (x * SQUARE_SIZE) - (((min_x + 1) * SQUARE_SIZE) / 2);
+				int rendery = box.y + ((SHAPE_BOUNDS_PX - piece_height) / 2) + (y * SQUARE_SIZE) - (((min_y + 1) * SQUARE_SIZE) / 2);
 				renderx -= ((min_x + 1) * SQUARE_SIZE) / 2;
 				rendery -= ((min_y + 1) * SQUARE_SIZE) / 2;
 				render_square(renderer, t->held_tetromino.squares[y][x], renderx, rendery, false);
@@ -191,17 +191,17 @@ static void tetris_render_hold(SDL_Renderer *renderer, const struct tetris *t)
 	}
 }
 
-static void tetris_render_queue(SDL_Renderer *renderer, const struct tetris *t)
+static void render_queue(SDL_Renderer *renderer, const struct tetris *t)
 {
 	SDL_SetRenderDrawColor(renderer, sidebar_bg[0], sidebar_bg[1], sidebar_bg[2], 255);
 	clear_current_viewport(renderer, &queue_viewport);
 
 	for (int i = 0; i < QUEUE_LENGTH; i++) {
 		SDL_Rect box = {
-			.x = (VIEWPORT_HOLD_WIDTH - PIECEBOX_WIDTH) / 2,
-			.y = ((WINDOW_HEIGHT - PIECEBOX_HEIGHT) / 4) + (PIECEBOX_HEIGHT * i),
-			.w = PIECEBOX_WIDTH,
-			.h = PIECEBOX_HEIGHT
+			.x = (VIEWPORT_HOLD_WIDTH - SHAPE_BOUNDS_PX) / 2,
+			.y = ((WINDOW_HEIGHT - SHAPE_BOUNDS_PX) / 4) + (SHAPE_BOUNDS_PX * i),
+			.w = SHAPE_BOUNDS_PX,
+			.h = SHAPE_BOUNDS_PX
 		};
 		SDL_SetRenderDrawColor(renderer, piecebox_bg[0], piecebox_bg[1], piecebox_bg[2], 255);
 		SDL_RenderFillRect(renderer, &box);
@@ -213,8 +213,8 @@ static void tetris_render_queue(SDL_Renderer *renderer, const struct tetris *t)
 				int min_y = tetromino_min_y(curr);
 				int piece_width = (tetromino_max_x(curr) - min_x) * SQUARE_SIZE;
 				int piece_height = (tetromino_max_y(curr) - min_y) * SQUARE_SIZE;
-				int renderx = box.x + ((PIECEBOX_WIDTH - piece_width) / 2) + (x * SQUARE_SIZE) - (((min_x + 1) * SQUARE_SIZE) / 2);
-				int rendery = box.y + ((PIECEBOX_HEIGHT - piece_height) / 2) + (y * SQUARE_SIZE) - (((min_y + 1) * SQUARE_SIZE) / 2);
+				int renderx = box.x + ((SHAPE_BOUNDS_PX - piece_width) / 2) + (x * SQUARE_SIZE) - (((min_x + 1) * SQUARE_SIZE) / 2);
+				int rendery = box.y + ((SHAPE_BOUNDS_PX - piece_height) / 2) + (y * SQUARE_SIZE) - (((min_y + 1) * SQUARE_SIZE) / 2);
 				renderx -= ((min_x + 1) * SQUARE_SIZE) / 2;
 				rendery -= ((min_y + 1) * SQUARE_SIZE) / 2;
 				render_square(renderer, curr->squares[y][x], renderx, rendery, false);
@@ -223,7 +223,7 @@ static void tetris_render_queue(SDL_Renderer *renderer, const struct tetris *t)
 	}
 }
 
-static void tetris_render_playfield(SDL_Renderer *renderer, const struct tetris *t)
+static void render_playfield(SDL_Renderer *renderer, const struct tetris *t)
 {
 	if (t->gameover) {
 		SDL_SetRenderDrawColor(renderer, gameover_bg[0], gameover_bg[1], gameover_bg[2], 255);
@@ -238,18 +238,18 @@ static void tetris_render_playfield(SDL_Renderer *renderer, const struct tetris 
 		}
 	}
 
-	tetris_render_current(renderer, t);
-	tetris_render_ghost(renderer, t);
+	render_current(renderer, t);
+	render_ghost(renderer, t);
 }
 
 void tetris_render(SDL_Renderer *renderer, const struct tetris *t)
 {
 	SDL_RenderSetViewport(renderer, &hold_viewport);
-	tetris_render_hold(renderer, t);
+	render_hold(renderer, t);
 
 	SDL_RenderSetViewport(renderer, &playfield_viewport);
-	tetris_render_playfield(renderer, t);
+	render_playfield(renderer, t);
 
 	SDL_RenderSetViewport(renderer, &queue_viewport);
-	tetris_render_queue(renderer, t);
+	render_queue(renderer, t);
 }
