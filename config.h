b44c243f6c5fe057e481bc105d485848e11c862d
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-10-26 07:48:36 -0400

    change gameover color

--- config.h
+++ config.h
@@ -27,7 +27,7 @@
 #define RED                (0xE23222)
 #define PURPLE             (0x780BB7)
 
-#define GAMEOVER_COLOR     (0x300A0A)
+#define GAMEOVER_COLOR     (0x851212)
 #define SIDEBARS_BG_COLOR  (0x373737)
 #define PIECEBOX_BG_COLOR  (0x000000)
 
